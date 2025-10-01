d670422d348cb3ad0a744bc52c6bbbde997de4aa
PeDitX <pedram.ale@me.com>
2025-10-01 05:14:11 +0330

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,21 @@
 import { defineCollection, z } from 'astro:content';
 
+// Define the schema for our "releases" collection to match the fields in config.yml
 const releasesCollection = defineCollection({
-  // Type-check frontmatter using a schema
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    // Transform string to Date object
-    pubDate: z
-      .string()
-      .or(z.date())
-      .transform((val) => new Date(val)),
-    heroImage: z.string(),
-    scriptUrl: z.string().url(),
-  }),
+	type: 'content', // This specifies that the collection contains markdown/mdx files
+	
+    // This schema validates the frontmatter of each markdown file
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// z.coerce will transform the date string from your markdown into a JavaScript Date object
+		pubDate: z.coerce.date(),
+		heroImage: z.string().optional(), // The path to the image is a string. .optional() means it's not required.
+		scriptUrl: z.string().optional(), // The script URL is also a string and is optional.
+	}),
 });
 
-export const collections = { 'releases': releasesCollection };
+// Export a `collections` object containing all our defined collections
+export const collections = {
+  'releases': releasesCollection,
+};
