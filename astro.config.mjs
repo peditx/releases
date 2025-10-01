dd3cd9dd65b1f59d689c34eb0f2c4963cdf49443
PeDitX <pedram.ale@me.com>
2025-10-01 06:59:06 +0330

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	// THE FINAL FIX IS HERE:
+	// We are setting the correct URL for your site.
+	// This allows Astro's build process to correctly resolve all file paths.
+	site: 'https://codes.peditxos.ir',
 	integrations: [mdx(), sitemap()],
 });
